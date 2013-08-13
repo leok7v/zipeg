e5129738fce740ed709b22da63112cfca8008914
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2013-08-13 00:29:44 -0700

    simplified ZGOOM creation

--- zipeg-mac/zipeg-ns/ZGErrors.m
+++ zipeg-mac/zipeg-ns/ZGErrors.m
@@ -9,7 +9,6 @@ NSError* ZGOutOfMemoryError() {
 }
 
 void ZGErrorsInit() {
-    NSMutableDictionary *details = [NSMutableDictionary dictionary];
-    [details setValue:ZG_ERROR_LOCALIZED_DESCRIPTION(ZGOutOfMemory) forKey:NSLocalizedDescriptionKey];
-    ZGOOM = [NSError errorWithDomain:ZGAppErrorDomain code:ZGIsNotAFile userInfo:details];
+    ZGOOM = [NSError errorWithDomain: ZGAppErrorDomain code: ZGIsNotAFile userInfo:
+             @{ NSLocalizedDescriptionKey: ZG_ERROR_LOCALIZED_DESCRIPTION(ZGOutOfMemory) }];
 }
